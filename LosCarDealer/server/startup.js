23ac0675103d59838fecd3dc60069e121aaca4ae
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-05 10:26:04 +0100

    Reworked CarDealer to use MongoDB

--- LosCarDealer/server/startup.js
+++ LosCarDealer/server/startup.js
@@ -1,16 +1,7 @@
 /// <reference types="@altv/types-server" />
 import * as alt from 'alt-server';
-import mysql from 'mysql2';
-
-let pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'keiner',
-    password: 'qS*qD7tc@cv#aJtu',
-    database: 'altv',
-    waitForConnections: true,
-    connectionLimit: 150,
-    queueLimit: 0,
-});
+import MongoClient from 'mongodb';
+let url = "mongodb://keiner:Gommekiller93@127.0.0.1:27017/";
 
 import './resourceStart';
 
@@ -29,15 +20,17 @@ function entityLeaveColshape(colshape, entity) {
     alt.emitClient(entity, 'CarDealer:leave');
 }
 
-alt.onClient('CarDealer:buyCar', async (player, carName) => {
+alt.onClient('CarDealer:buyCar', (player, carName) => {
     try {
         if (player.getStreamSyncedMeta('lastVehicle') && player.getStreamSyncedMeta('lastVehicle').valid) {
             player.getStreamSyncedMeta('lastVehicle').destroy();
             player.deleteStreamSyncedMeta('lastVehicle');
         }
 
-        let vehicle = await new alt.Vehicle(
-            carName,
+        let carHash = alt.hash(carName);
+
+        let vehicle = new alt.Vehicle(
+            carHash,
             -31.094505310058594,
             -1089.20654296875,
             25.859375,
@@ -45,77 +38,73 @@ alt.onClient('CarDealer:buyCar', async (player, carName) => {
             0,
             -0.346317321062088
         );
+        vehicle.dimension = 1;
         player.setStreamSyncedMeta('lastVehicle', vehicle);
 
         vehicle.setSyncedMeta('tank', 100);
         vehicle.setSyncedMeta('engine', false);
-        vehicle.setSyncedMeta('vehicleLock', 1);
+        vehicle.lockState = 1;
         let numberPlate = makeNumberPlate(8);
         vehicle.numberPlateText = numberPlate;
-        pool.getConnection(function (err, conn) {
+        MongoClient.connect(url,  {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
             if (err) throw err;
-            conn.execute(
-                'SELECT garage FROM `character` WHERE socialId=?',
-                [player.socialId],
-                function (err, garageData, fields) {
-                    if (err) throw err;
-                    let garage = JSON.parse(garageData[0]["garage"]);
-                    if (garage.some(item => item.hash === vehicle.model)) {
-                        vehicle.destroy();
-                        pool.releaseConnection(conn);
-                        return;
-                    }
-                    garage.push({
-                        name: carName,
-                        hash: vehicle.model,
-                        tank: 100,
-                        numberplate: numberPlate,
-                        parking: false,
-                        dirtLevel: vehicle.dirtLevel,
-                        damage: {
-                            bodyAdditionalHealth: vehicle.bodyAdditionalHealth,
-                            bodyHealth: vehicle.bodyHealth,
-                            engineHealth: vehicle.engineHealth,
-                            petrolTankHealth: vehicle.petrolTankHealth,
-                            healthDataBase64: vehicle.getHealthDataBase64()
-                        },
-                        tuning: {
-                            modkit: vehicle.modKit,
-                            optic: {
-                                customTires: vehicle.customTires,
-                                dashboardColor: vehicle.dashboardColor,
-                                headlightColor: vehicle.headlightColor,
-                                interiorColor: vehicle.interiorColor,
-                                neon: vehicle.neon,
-                                neonColor: vehicle.neonColor,
-                                primaryColor: vehicle.primaryColor,
-                                secondaryColor: vehicle.secondaryColor,
-                                pearlColor: vehicle.pearlColor,
-                                tireSmokeColor: vehicle.tireSmokeColor,
-                                wheelColor: vehicle.wheelColor
-                            },
-                            peformance: {
-                                brakes: vehicle.getMod(12),
-                                engine: vehicle.getMod(11),
-                                spoiler: vehicle.getMod(0),
-                                suspension: vehicle.getMod(15),
-                                transmission: vehicle.getMod(13),
-                                turbo: vehicle.getMod(18)
-                            }
-                        }
-                    });
-                    conn.execute(
-                        'UPDATE `character` SET garage=? WHERE socialId=?',
-                        [JSON.stringify(garage), player.socialId],
-                        function (err, res, fields) {
-                            if (err) throw err;
-                            alt.emitClient(player, 'setPedIntoVehicle', vehicle);
-                            alt.emit('getGarage');
-                        }
-                    );
+            let database = db.db('altv');
+            database.collection('accounts').findOne({ socialclub: player.socialID }, function(err, result) {
+                if (err) throw err;
+                if (result === null || result.length <= 0) {
+                    vehicle.destroy();
+                    return;
                 }
-            );
-            pool.releaseConnection(conn);
+                let garage = result.garage;
+                if (garage.some(item => item.hash === vehicle.model)) {
+                    vehicle.destroy();
+                    return;
+                }
+                garage.push({
+                    name: carName,
+                    hash: vehicle.model,
+                    tank: 100,
+                    numberplate: numberPlate,
+                    parking: false,
+                    dirtLevel: vehicle.dirtLevel,
+                    damage: {
+                        bodyAdditionalHealth: vehicle.bodyAdditionalHealth,
+                        bodyHealth: vehicle.bodyHealth,
+                        engineHealth: vehicle.engineHealth,
+                        petrolTankHealth: vehicle.petrolTankHealth,
+                        healthDataBase64: vehicle.getHealthDataBase64()
+                    },
+                    tuning: {
+                        modkit: vehicle.modKit,
+                        optic: {
+                            customTires: vehicle.customTires,
+                            dashboardColor: vehicle.dashboardColor,
+                            headlightColor: vehicle.headlightColor,
+                            interiorColor: vehicle.interiorColor,
+                            neon: vehicle.neon,
+                            neonColor: vehicle.neonColor,
+                            primaryColor: vehicle.primaryColor,
+                            secondaryColor: vehicle.secondaryColor,
+                            pearlColor: vehicle.pearlColor,
+                            tireSmokeColor: vehicle.tireSmokeColor,
+                            wheelColor: vehicle.wheelColor
+                        },
+                        peformance: {
+                            brakes: vehicle.getMod(12),
+                            engine: vehicle.getMod(11),
+                            spoiler: vehicle.getMod(0),
+                            suspension: vehicle.getMod(15),
+                            transmission: vehicle.getMod(13),
+                            turbo: vehicle.getMod(18)
+                        }
+                    }
+                });
+                database.collection('accounts').updateOne({ socialclub: player.socialID }, { $set: { garage: garage }}, function (err, result) {
+                    if (err) throw err;
+                    alt.emitClient(player, 'setPedIntoVehicle', vehicle);
+                    alt.emit('getGarage');
+                });
+            });
         });
     } catch (err) {
         console.log(err);
