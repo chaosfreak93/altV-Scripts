3d02143c7e1c07320f2663f82110fa5af5f5467a
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-05 09:54:31 +0100

    Renamed LosRadio to LosVehicleRadio and changed funtion of LosRadio

---
+++ LosRadio/client/html/app.js
@@ -0,0 +1,14 @@
+/// <reference types="@altv/types-natives" />
+Vue.config.devtools = true;
+Vue.prototype.window = window;
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            message: '',
+        }
+    },
+    methods: {},
+    mounted() {},
+});
---
+++ LosRadio/client/keyup.js
@@ -0,0 +1,23 @@
+/// <reference types="@altv/types-client" />
+/// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+
+let view = null;
+
+alt.on('keyup', (key) => {
+    switch(key) {
+        case 117:
+            if (!view) {
+                view = new alt.WebView("http://resource/client/html/index.html");
+                view.focus();
+
+                alt.showCursor(true);
+            } else {
+                view.destroy();
+                view = null;
+                
+                alt.showCursor(false);
+            }
+            break;
+    }
+})
