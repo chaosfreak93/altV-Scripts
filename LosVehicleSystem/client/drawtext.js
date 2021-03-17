803892ed7e66b761a13a17f435f139ab8a6c2808
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-17 08:22:24 +0100

    Vehicle Hud Cleanup

--- LosVehicleSystem/client/drawtext.js
+++ LosVehicleSystem/client/drawtext.js
@@ -11,59 +11,39 @@ let _speed;
 let _engine;
 let _tank;
 let _engineOn;
-let _isEngineOn;
 
 
-alt.on('enteredVehicle', _enterVeh);
-alt.on('leftVehicle', _leaveVeh);
-alt.onServer('player:leftVehicle', _leaveVeh);
-
-function _leaveVeh() {
-    alt.clearEveryTick(_getTank);
-    alt.clearEveryTick(_isEngineOn);
-    alt.clearInterval(_speedTick);
-    alt.clearEveryTick(_engine);
-    alt.clearInterval(_Tank);
-    alt.clearEveryTick(_display);
-    native.displayRadar(false);
-}
-
-alt.onServer('getTank', (tank) => {
-    _tank = tank;
-});
-
-alt.onServer('isEngineRunning', (engineOn) => {
-    _engineOn = engineOn;
-});
-
-function _enterVeh(Vehicle) {
+alt.onServer('playerEnteredVehicle', (vehicle, seat) => {
     native.displayRadar(true);
     _getTank = alt.everyTick(() => {
-        if (Vehicle !== null && Vehicle.valid) {
-            alt.emitServer('getTank', Vehicle);
-            alt.emitServer('isEngineRunning', Vehicle);
+        if (vehicle !== null && vehicle.valid) {
+            alt.emitServer('getTank', vehicle);
+            alt.emitServer('isEngineRunning', vehicle);
         }
     });
 
     _speedTick = alt.setInterval(() => {
-        if (Vehicle !== null && Vehicle.valid) {
-            _speed = Math.round(native.getEntitySpeed(Vehicle.scriptID) * 3.6);
+        if (vehicle !== null && vehicle.valid) {
+            _speed = Math.round(native.getEntitySpeed(vehicle.scriptID) * 3.6);
         }
     }, 100);
 
     _engine = alt.everyTick(() => {
-        if (Vehicle !== null && Vehicle.valid) {
+        if (vehicle !== null && vehicle.valid) {
             if (_tank <= 0) {
-                native.setVehicleEngineOn(Vehicle.scriptID, false, false, true);
+                native.setVehicleEngineOn(vehicle.scriptID, false, false, true);
             }
         }
     });
     _Tank = alt.setInterval(() => {
-        if (Vehicle !== null && Vehicle.valid) {
-            if (_tank >= 1) {
-                if (_engineOn) {
-                    let tank = _tank - 1;
-                    alt.emitServer('setTank', Vehicle, tank);
+        if (vehicle !== null && vehicle.valid) {
+            let driver = native.getPedInVehicleSeat(vehicle.scriptID, -1, false);
+            if (alt.Player.local.scriptID === driver) {
+                if (_tank >= 1) {
+                    if (_engineOn) {
+                        let tank = _tank - 1;
+                        alt.emitServer('setTank', vehicle, tank);
+                    }
                 }
             }
         }
@@ -75,7 +55,24 @@ function _enterVeh(Vehicle) {
         drawText2d('Tank: ' + tank + '%', 0.85, 0.85, 1, 255, 255, 255, 255);
         drawText2d('Engine: ' + engine, 0.85, 0.9, 1, 255, 255, 255, 255);
     });
-}
+});
+
+alt.onServer('playerLeftVehicle', (vehicle, seat) => {
+    alt.clearEveryTick(_getTank);
+    alt.clearInterval(_speedTick);
+    alt.clearEveryTick(_engine);
+    alt.clearInterval(_Tank);
+    alt.clearEveryTick(_display);
+    native.displayRadar(false);
+});
+
+alt.onServer('getTank', (tank) => {
+    _tank = tank;
+});
+
+alt.onServer('isEngineRunning', (engineOn) => {
+    _engineOn = engineOn;
+});
 
 function hexToRgb(hex) {
     var bigint = parseInt(hex, 16);
