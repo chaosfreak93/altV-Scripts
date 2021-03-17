0c6a04691097128cd7931f3581a3a7f9550160ed
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-17 08:36:38 +0100

    Reworked LosCore JS to BeyondCore C#

--- LosCore/server/startup.js
+++
@@ -1,8 +0,0 @@
-// Events
-import './events/playerConnect';
-import './events/playerDeath';
-import './events/playerDisconnect';
-import './database/register';
-
-// Prototypes
-import './prototypes/player';
