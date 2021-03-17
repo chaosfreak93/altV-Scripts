89b3f0abfd71577aa77cb097a1ed50125f1f5965
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-17 08:37:07 +0100

    Moved LosJobs to BeyondCore

--- LosJobs/server/database/salary.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="@altv/types-server" />
-import * as alt from 'alt-server';
-import money from 'LosMoney';
-
-alt.onClient('salary', salary);
-
-function salary(player) {
-    money.api.addMoneyToBank(player, player.getMeta('salary'));
-}
