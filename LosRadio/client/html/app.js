f32af30e43310f6f4abfafb1da93af575cff7b16
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-17 08:46:02 +0100

    Added a Radio Prototype

--- LosRadio/client/html/app.js
+++ LosRadio/client/html/app.js
@@ -6,10 +6,24 @@ const app = new Vue({
     el: '#app',
     data() {
         return {
-            message: '',
+            channel: 1,
         }
     },
-    methods: {},
+    methods: {
+        joinChannel() {
+            this.channel = parseInt(this.channel + "");
+            if (this.channel > 100) {
+                this.channel = 100;
+            }
+            if (this.channel < 1) {
+                this.channel = 1;
+            }
+            alt.emit("joinChannel", this.channel + "");
+        },
+        leaveChannel() {
+            alt.emit("leaveChannel");
+        }
+    },
     mounted() {
     },
 });
--- LosRadio/client/keyup.js
+++ LosRadio/client/keyup.js
@@ -1,23 +1,58 @@
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-natives" />
 import * as alt from 'alt-client';
+import * as native from 'natives';
 
 let view = null;
+let controlAction = null;
 
 alt.on('keyup', (key) => {
-    switch (key) {
-        case 117:
-            if (!view) {
-                view = new alt.WebView("http://resource/client/html/index.html");
-                view.focus();
+    if (alt.gameControlsEnabled()) {
+        switch (key) {
+            case 117:
+                if (!view) {
+                    view = new alt.WebView("http://resource/client/html/index.html");
+                    view.on("joinChannel", joinChannel);
+                    view.on("leaveChannel", leaveChannel);
+                    view.focus();
 
-                alt.showCursor(true);
-            } else {
-                view.destroy();
-                view = null;
+                    alt.showCursor(true);
+                    controlAction = alt.everyTick(() => {
+                        native.disableControlAction(0, 24, true);
+                        native.disableControlAction(0, 25, true);
+                        native.disableControlAction(0, 37, true);
+                        native.disableControlAction(0, 68, true);
+                        native.disableControlAction(0, 69, true);
+                        native.disableControlAction(0, 70, true);
+                        native.disableControlAction(0, 91, true);
+                        native.disableControlAction(0, 92, true);
+                        native.disableControlAction(0, 114, true);
+                        native.disableControlAction(0, 142, true);
+                        native.disableControlAction(0, 199, true);
+                        native.disableControlAction(0, 200, true);
+                        native.disableControlAction(0, 257, true);
+                        native.disableControlAction(0, 270, true);
+                        native.disableControlAction(0, 271, true);
+                        native.disableControlAction(0, 272, true);
+                        native.disableControlAction(0, 273, true);
+                        native.disableControlAction(0, 331, true);
+                    });
+                } else {
+                    view.destroy();
+                    view = null;
 
-                alt.showCursor(false);
-            }
-            break;
+                    alt.showCursor(false);
+                    alt.clearEveryTick(controlAction);
+                }
+                break;
+        }
     }
-})
+});
+
+function joinChannel(channel) {
+    alt.emitServer("joinChannel", channel);
+}
+
+function leaveChannel() {
+    alt.emitServer("leaveChannel");
+}
