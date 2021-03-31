63b87bc660cdebcd6ec31239869af85a2892f786
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-03-31 23:05:36 +0200

    Removed Discord Bot

--- LosDiscord/server/events/playerConnect.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="@altv/types-server" />
-import * as alt from 'alt-server';
-import {playerJoin} from '../discord';
-
-alt.on('playerConnect', playerConnect);
-
-function playerConnect(player) {
-    playerJoin(player.name);
-}
--- LosDiscord/server/events/resourceStop.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="@altv/types-server" />
-import * as alt from 'alt-server';
-import {stop} from '../discord';
-
-alt.on('anyResourceStop', resourceStop);
-
-function resourceStop(resourceName) {
-    stop();
-}
