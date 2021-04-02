b1b65af54c384ce5d22abb950420f4d4fa6741f4
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-04-02 23:26:20 +0200

    Reworked Login UI

--- LosDiscordAuth/server/html/js/app.js
+++ LosDiscordAuth/server/html/js/app.js
@@ -3,12 +3,7 @@ Vue.prototype.window = window;
 
 const app = new Vue({
     el: '#app',
-    data() {
-        return {
-            info: `Einloggen erfolgreich. Du kannst das Fenster jetzt schließen. Viel Spaß im RP!`,
-        };
-    },
     mounted() {
-        window.history.replaceState(null, null, window.location.pathname);
+        history.replaceState(null, '', window.location.pathname);
     },
 });
